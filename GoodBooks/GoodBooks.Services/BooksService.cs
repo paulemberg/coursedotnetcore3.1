f4debe9ab13d8c1e3c2af0e5006b65bda481cb01
Berg <paulemberg@gmail.com>
2020-05-09 13:15:22 -0300

    update: .gitignore removing .idea files, fix references of project

--- GoodBooks/GoodBooks.Services/BooksService.cs
+++ GoodBooks/GoodBooks.Services/BooksService.cs
@@ -11,13 +11,11 @@ namespace GoodBooks.Services
         {
             _db = db;
         }
-
         public void AddBook(Book book)
         {
             _db.Add(book);
             _db.SaveChanges();
         }
-
         public void DeleteBook(int bookId)
         {
             var bookToDelete = _db.Books.Find(bookId);
